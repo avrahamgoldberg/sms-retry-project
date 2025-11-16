71f81c41e6d4037d49288f31b34e4f2e86945fdf
avrahamgoldberg <avrahamg9@gmail.com>
2025-11-16 09:18:52 +0200

    Stop tracking .env

--- src/config.py
+++ src/config.py
@@ -11,8 +11,8 @@ class Config:
 
     # AWS Configuration
     AWS_REGION: str = os.getenv('AWS_REGION', 'us-east-1')
-    AWS_ACCESS_KEY_ID: str = os.getenv('AWS_ACCESS_KEY_ID', 'test')
-    AWS_SECRET_ACCESS_KEY: str = os.getenv('AWS_SECRET_ACCESS_KEY', 'test')
+    AWS_ACCESS_KEY_ID: str = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY: str = os.getenv('AWS_SECRET_ACCESS_KEY')
 
     # S3 Configuration
     S3_BUCKET: str = os.getenv('S3_BUCKET', 'sms-retry-scheduler')
